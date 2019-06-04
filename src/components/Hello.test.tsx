75970c49d8ae819d35563c8d43887aee5cacd585
Daniel Bushman <me@danielbushman.com>
2019-06-04 17:16:25 -0600

    Add Hello component and tests

---
+++ src/components/Hello.test.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { shallow } from "enzyme";
+import Hello from "./Hello";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it("renders the correct text when no enthusiasm level is given", () => {
+  const hello = shallow(<Hello name="Daniel" />);
+  expect(hello.find(".greeting").text()).toEqual("Hello Daniel!");
+});
+
+it("renders the correct text with an explicit enthusiasm of 1", () => {
+  const hello = shallow(<Hello name="Daniel" enthusiasmLevel={1} />);
+  expect(hello.find(".greeting").text()).toEqual("Hello Daniel!");
+});
+
+it("renders the correct text with an explicit enthusiasm level of 5", () => {
+  const hello = shallow(<Hello name="Daniel" enthusiasmLevel={5} />);
+  expect(hello.find(".greeting").text()).toEqual("Hello Daniel!!!!!");
+});
+
+it("throws when the enthusiasm level is 0", () => {
+  expect(() => {
+    shallow(<Hello name="Daniel" enthusiasmLevel={0} />);
+  }).toThrow();
+});
+
+it("throws when the enthusiasm level is negative", () => {
+  expect(() => {
+    shallow(<Hello name="Daniel" enthusiasmLevel={-1} />);
+  }).toThrow();
+});
---
+++ src/components/Hello.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import "./Hello.css";
+
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+}
+
+interface State {
+  currentEnthusiasm: number;
+}
+
+class Hello extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      currentEnthusiasm:
+        props.enthusiasmLevel === undefined ? 1 : props.enthusiasmLevel
+    };
+  }
+
+  onIncrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
+  onDecrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
+
+  updateEnthusiasm(currentEnthusiasm: number) {
+    this.setState({ currentEnthusiasm });
+  }
+
+  render() {
+    const { name } = this.props;
+
+    if (this.state.currentEnthusiasm <= 0) {
+      throw new Error("Be more enthusiastic!");
+    }
+
+    return (
+      <div className="hello">
+        <div className="greeting">
+          Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
+        </div>
+        <button onClick={this.onIncrement}>+</button>
+        <button onClick={this.onDecrement}>-</button>
+      </div>
+    );
+  }
+}
+
+export default Hello;
+
+// helpers
+
+const getExclamationMarks = (numChars: number) => {
+  return Array(numChars + 1).join("!");
+};
